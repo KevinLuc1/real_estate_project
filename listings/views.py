ead9bd3bddb3e4ae28a0a0ccd5eba21b97ea06e1
Kevin <kevinluc1993@gmail.com>
2021-03-28 23:26:55 -0700

    listings are now dynamically fetched from db, added pagination

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+from .models import Listing
 
 def index(request):
-   return render(request, 'listings/listings.html')
+    #Fetch our listings using our model and then we insert our template, loop through and output the listing that are in the DB
+    listings = Listing.objects.all().filter(is_published=True)
 
-def listing(request):
+    # search paginator from django docs
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+
+    # we can either pass in a dictionary as a 2nd param in render
+    # ie: {'name': 'Kevin:} and inside html we use {{ name }}
+    #w we can instead create a variable as a dictionary and pass in the variable 'context'
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
