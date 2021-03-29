dcacfd99f131bfbd9e77677c9ce2bf34234433dd
Kevin <kevinluc1993@gmail.com>
2021-03-28 21:36:08 -0700

    customized django admin page with built in methods

--- listings/admin.py
+++ listings/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 
+#sends this into the django admin
 from .models import Listing
 
-admin.site.register(Listing)
+#We can add certain variables here or properties that relate to how things are displayed
+# we can customize our django admin with methods here without to instead of  having to go into the django admin code
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor') #choose items to display in admin
+    list_display_links = ('id', 'title') # allows you to choose which categories are clickable
+    list_filter = ('realtor', ) # create a filer for said category
+    list_editable = ('is_published', ) #turn true false into clickbale check box
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price') #add searchable field
+    list_per_page = 20
+
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -5,4 +5,12 @@ from django.contrib import admin
 #sends this into the django admin
 from .models import Realtor
 
-admin.site.register(Realtor)
+#We can add certain variables here or properties that relate to how things are displayed
+# we can customize our django admin with methods here without to instead of  having to go into the django admin code
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date') #choose items to display in admin page
+    list_display_links = ('id', 'name') #choose which links are clickable to open file
+    search_fields = ('name', ) #adds a box to search based off 'name'
+    list_per_page = 20
+
+admin.site.register(Realtor, RealtorAdmin)
