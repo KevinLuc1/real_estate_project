3ace68658b60e590b5d4706ce88c5547b12c3f5f
Kevin <kevinluc1993@gmail.com>
2021-04-12 21:50:54 -0700

    static root

--- real_estate_project/settings.py
+++ real_estate_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')a)yai85b!@*b=^d_1_pc4wof!c5569&m3s&nj%iu263f+l@cf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'django-project-real-estate.herokuapp.com']
 
