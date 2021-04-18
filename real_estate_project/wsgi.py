b105c722d6a1612c96598a682ceda0b97cb3aa4d
Kevin <kevinluc1993@gmail.com>
2021-04-18 15:07:05 -0700

    edit wsgi

--- real_estate_project/wsgi.py
+++ real_estate_project/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhitenoise
+# from whitenoise.django import DjangoWhitenoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_estate_project.settings')
 
 application = get_wsgi_application()
-application = DjangoWhitenoise(application)
+# application = DjangoWhitenoise(application)
